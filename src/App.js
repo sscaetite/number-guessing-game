83f2b7d80d32486b05163b94ecc40e73df58ac82
samuelcaetite <samuel.silva@driven.cx>
2021-10-10 15:18:58 -0300

    chore: configuração do react e suas dependências

---
+++ src/App.js
@@ -0,0 +1,19 @@
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Em construção.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
