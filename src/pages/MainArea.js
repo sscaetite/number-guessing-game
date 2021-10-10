524a2a2f76a4741130b33cdf7e7d4571d717883e
samuelcaetite <samuel.silva@driven.cx>
2021-10-10 16:19:37 -0300

    feat: adição do Text component

--- src/pages/MainArea.js
+++ src/pages/MainArea.js
@@ -1,10 +1,13 @@
 import GlassContainer from "../components/GlassContainer";
 import MainContainer from "../components/MainContainer";
+import Text from "../components/Text";
 
 const MainArea = () => {
   return (
     <MainContainer>
-      <GlassContainer>Main area</GlassContainer>
+      <GlassContainer>
+        <Text>Main Area</Text>
+      </GlassContainer>
     </MainContainer>
   );
 };
